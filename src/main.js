e891c979ae6a560d18b8e81bb842167889c1bdd4
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2023-08-03 00:40:26 +0300

    create filter component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import NewTaskBtn from './view/new-task.js';
+import Filter from './view/filter.js';
 import {render} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 render(new NewTaskBtn, siteHeaderElement);
+render(new Filter, siteMainElement)
