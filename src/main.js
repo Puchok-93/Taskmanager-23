bab1aec9c4b4193f07351880fe8c7c037ae7c59a
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2023-08-03 18:29:23 +0300

    create another components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import NewTaskBtn from './view/new-task.js';
 import Filter from './view/filter.js';
+import Sorting from './view/sorting.js';
+import TaskList from './view/task-list.js';
+import Task from './view/task.js';
+import TaskEdit from './view/task-edit.js';
+import LoadMoreButton from './view/load-more.js';
 import {render} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 render(new NewTaskBtn, siteHeaderElement);
-render(new Filter, siteMainElement)
+render(new Filter, siteMainElement);
+render(new Sorting, siteMainElement);
+render(new Task, siteMainElement);
+render(new TaskEdit, siteMainElement);
+render(new LoadMoreButton, siteMainElement);
--- src/view/new-task.js
+++ src/view/new-task.js
@@ -1,22 +1,23 @@
 import {createElement} from '../render.js'
 
 
-const createNewTaskBtnTemplate = () => ` <button class="control__button">+ ADD NEW TASK</button>`;
+const createNewTaskBtnTemplate = () => `<button class="control__button">+ ADD NEW TASK</button>`;
 
 export default class NewTaskBtn {
-    getTemplate() {
-        return createNewTaskBtnTemplate();
+
+    getTemplate() { // Получаем шаблон разметки
+      return createNewTaskBtnTemplate(); // Возвращаем полученный шаблон
+    }
+    
+    getElement() { // Получаем элемент
+      if (!this.element) { // Если элемента нет
+        this.element = createElement(this.getTemplate()); // Создаем элемент, получив его шаблон
       }
     
-      getElement() {
-        if (!this.element) {
-          this.element = createElement(this.getTemplate());
-        }
+      return this.element; // Если элемента ест, возвращаем его
+    }
     
-        return this.element;
-      }
-    
-      removeElement() {
-        this.element = null;
-      }
+    removeElement() {
+      this.element = null;
+    }
 }
