b7a69522163b1212940d129ef6412ec84d80ae6b
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2023-08-06 21:05:11 +0300

    create board-presenter

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 import NewTaskBtn from './view/new-task.js';
 import Filter from './view/filter.js';
-import Sorting from './view/sorting.js';
-import TaskList from './view/task-list.js';
-import Task from './view/task.js';
-import TaskEdit from './view/task-edit.js';
-import LoadMoreButton from './view/load-more.js';
+import BoardPresenter from './presenter/board-presenter.js';
 import {render} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const boardPresenter = new BoardPresenter();
 
 render(new NewTaskBtn, siteHeaderElement);
 render(new Filter, siteMainElement);
-render(new Sorting, siteMainElement);
-render(new Task, siteMainElement);
-render(new TaskEdit, siteMainElement);
-render(new LoadMoreButton, siteMainElement);
+
+boardPresenter.init(siteMainElement);
